7b7d0dee038e85f8fb1a8aa85f5a1cc40877547f
Mangalabankar13 <138298410+Mangalabankar13@users.noreply.github.com>
2023-07-04 14:23:05 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ int i,n,a[20],b[20];
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter element");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ for(i=0;i<n;i++)
+ {
+  b[i]=a[i]*a[i];
+ }
+ printf("\nfirst array:");
+ for(i=0;i<n;i++)
+ printf("%d ",a[i]);
+ printf("\nsecond square array:");
+ for(i=0;i<n;i++)
+ printf("%d ",b[i]);
+}    
